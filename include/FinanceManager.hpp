0d910b947e7eec83b49cc84403e9ddbf4d0b0272
leonardoyf26 <leonardoyf26@gmail.com>
2025-12-04 20:02:44 -0300

    implement FinanceManager header

---
+++ include/FinanceManager.hpp
@@ -0,0 +1,18 @@
+#ifndef FINANCEMANAGER_H
+#define FINANCEMANAGER_H
+#include "Transaction.hpp"
+#include <string>
+#include <vector>
+
+class FinanceManager
+{
+private:
+    std::vector<Transaction> transactions{};
+
+public:
+    void addTransaction(const Transaction& t);
+    void listAll() const;
+    void listByMonth(std::string d1) const;
+};
+
+#endif
